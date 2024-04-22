7bc19d10b0d55a2cb286c670bbd1159647fd0b6d
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2024-04-22 14:09:28 -0700

    made changes to turn it int o app academy canva

---
+++ problems/02-code-comments.js
@@ -0,0 +1,26 @@
+/*
+Use your newfound knowledge of comments to execute the prompts in this problem
+file.
+*/
+
+/* 1. This will print 'Welcome to App Academy!'  */
+
+console.log('Welcome to App Academy!');
+
+/* Print more messages to the terminal using console.log */
+
+// 2. This is a single line comment.
+
+// Try adding your own single line comments:
+
+ 
+// hello everyone
+/* 3.
+This is a multi-line comment.
+Try adding your own multi-line comments:
+*/
+
+// Your code here 
+/* Hello
+my name is 
+Kaiden */
